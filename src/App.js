aaefe8b7d5e42dd0118a614a5515376e7b574e79
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2022-10-25 11:40:29 +0100

    tailwind install snagging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,246 @@
 import './App.css';
+import { BsFillMoonStarsFill} from "react-icons/bs";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import bigFaceLogo from "./assets/bigfacetrans.png";
+import gym from "./assets/gym-app-screenshot.png";
+import simpsons from "./assets/simpsons-toptrumps-screenshot.png";
+import portfolio from "./assets/portfolio-screenshot.png";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const refForm = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_tiks2fi",
+        "template_pvr2pzj",
+        refForm.current,
+        "J8TMy_h-CK24L-gFG"
+      )
+      .then(
+        () => {
+          alert("Message successfully sent!");
+          window.location.reload();
+        },
+        () => {
+          alert("Failed to send the message, please try again");
+        }
+      );
+  };
+
   return (
-    <div>
+    <div className={darkMode ? "dark" : ""}>
+        <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-slate-900">
+        <section>
+          {/* navbar section need a hrefs */}
+          <nav className="py-10 flex justify-between">
+            <span></span>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  className="cursor-pointer text-2xl dark:text-white"
+                  onClick={() => setDarkMode(!darkMode)}
+                />
+              </li>
+              
+            </ul>
+          </nav>
 
+          {/* Intro */}
 
+          <div className=" text-center">
+            <h2 className=" text-4xl py-4 px-0 text-teal-600 font-medium md:text-6xl">
+              Ben Cutler-Ames
+            </h2>
+            <h3 className=" text-2xl py-2 md:text-3xl dark:text-white">
+              Software Developer
+            </h3>
+            <p className=" text-medium py-2 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200">
+              Software developer career-changer soon to be graduating from
+              CodeClan&apos;s Professional Software Development Course
+            </p>
+          </div>
+          {/* links */}
+          <div className=" text-5xl flex justify-center gap-20 py-3 text-gray-600 md:text-6xl dark:text-green-200">
+            <a href="https://github.com/BenCutlerAmes" target='_blank' rel='noreferrer' >
+              <AiFillGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/bencutlerames/" target='_blank' rel='noreferrer' >
+              <AiFillLinkedin />
+            </a>
+          </div>
+          {/* image */}
+          <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-2 md:w-96 md:h-96">
+            <img
+              src={bigFaceLogo}
+              alt="logo"
+              layout="fill"
+              objectFit="scale-down"
+            />
+          </div>
+        </section>
+
+        {/*  */}
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 text-center md:text-4xl md:py-3 dark:text-white">
+              About Me
+            </h3>
+            <p className=" text-medium py-2 leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-200">
+              I am career-changer currently studying at CodeClan in Edinburgh,
+              with a background as a Mathematics Teacher and Housemaster.
+            </p>
+            <p className="text-medium py-2 leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-200">
+              My previous experience, combined with my strong abilities in
+              analysis, leadership and people skills, have provided a strong
+              basis for this move into the technology sector.
+            </p>
+            <p className="text-medium py-2 leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-200">
+              With 2 major course projects down and one to go, I am seeking job
+              opportunities on completion of the course that enable me to apply
+              these skills and start my new career.
+            </p>
+          </div>
+        </section>
+        <section>
+          {/* portfolio pages - need to update links to github */}
+          <div>
+            <h3 className="text-3xl py-1 text-center md:text-4xl md:py-3 dark:text-white">
+              Portfolio
+            </h3>
+            <div className="lg:flex gap-10 justify-center">
+              <div className=" text-center shadow-2xl shadow-black p-10 rounded-xl my-10 dark:shadow-white dark:shadow-lg">
+                <a href="https://github.com/BenCutlerAmes/Gym_Management_App" target='_blank' rel='noreferrer'>
+                  <img src={gym} alt="screenshot" width={500} height={281} />
+                </a>
+                <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600">
+                  Gym Management App
+                </h3>
+                <p className="text-gray-800 py-1 dark:text-gray-200">Python</p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">
+                  PostgreSQL
+                </p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">Flask</p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">
+                  Psycopg2
+                </p>
+              </div>
+
+              <div className=" text-center shadow-2xl shadow-black p-10 rounded-xl my-10 dark:shadow-white dark:shadow-lg">
+                <a href="https://github.com/BenCutlerAmes/Simpsons-Top-Trumps" target='_blank' rel='noreferrer'>
+                  <img
+                    src={simpsons}
+                    alt="screenshot"
+                    width={500}
+                    height={281}
+                  />
+                </a>
+                <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600">
+                  Simpsons Top Trumps
+                </h3>
+                <p className="text-gray-800 py-1 dark:text-gray-200">
+                  JavaScript
+                </p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">React</p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">MongoDB</p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">Express</p>
+              </div>
+
+              <div className=" text-center shadow-2xl shadow-black p-10 rounded-xl my-10 dark:shadow-white dark:shadow-lg">
+                <a href="https://github.com/BenCutlerAmes/portfolio-website" target='_blank' rel='noreferrer'>
+                  <img
+                    src={portfolio}
+                    alt="screenshot"
+                    width={500}
+                    height={281}
+                  />
+                </a>
+                <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600">
+                  Portfolio Website
+                </h3>
+                <p className="text-gray-800 py-1 dark:text-gray-200">React</p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">
+                  Tailwind
+                </p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">
+                  JavaScript
+                </p>
+                <p className="text-gray-800 py-1 dark:text-gray-200">
+                  Firebase
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h3 className="text-3xl py-1 text-center md:text-4xl md:py-3 dark:text-white">
+            {" "}
+            Contact Me
+          </h3>
+          <p className="text-medium py-2 leading-8 text-gray-800 md:text-xl max-w-3xl mx-auto dark:text-gray-200">
+            I am interested in employment opportunities, mentorship or just
+            generally kind words. If you have these, or anything else to say ,
+            don&apos;t hesitate to contact me using the form below.
+          </p>
+          <form className="text-center" ref={refForm} onSubmit={sendEmail}>
+            <ul className="mx-auto">
+              <li className="mx-auto">
+                <input
+                  className="bg-blue-100 rounded-md py-1 px-3 w-96 mb-2 text-xl  md:text-2xl md:py-3"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  required
+                />
+              </li>
+              <li>
+                <input
+                  className="bg-blue-100 rounded-md py-1 px-3 w-96 mb-2 text-xl  md:text-2xl md:py-3"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                />
+              </li>
+              <li>
+                <input
+                  className="bg-blue-100 rounded-md py-1 px-3 w-96 mb-2 text-xl  md:text-2xl md:py-3"
+                  type="text"
+                  placeholder="Subject"
+                  name="subject"
+                  required
+                />
+              </li>
+              <li>
+                <textarea
+                  className="bg-blue-100 rounded-md py-1 px-3 w-96 mb-2 text-xl md:text-2xl md:py-3"
+                  placeholder="Message"
+                  name="message"
+                />
+              </li>
+              <li>
+                <input
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                  type="submit"
+                  value="SEND"
+                />
+              </li>
+            </ul>
+            <br />
+            <br />
+            <br />
+            <br />
+          </form>
+        </section>
+      </main>
     </div>
-      
   );
 }
 
